fc7239a1a6bb23ddd6eb36fa3977506f5b66f371
Adil Ahmed <asa7496@gmail.com>
2020-06-13 10:00:48 -0700

    submitting ETL project

---
+++ ETL Project/player.sql
@@ -0,0 +1,16 @@
+CREATE TABLE players(
+	Age VARCHAR,
+	Player_ID VARCHAR,
+	Field_Goal_Percentage FLOAT,
+	Free_Throw_Percentage FLOAT,
+	Total_Rebounds INT,
+	Assists INT,
+	Steals INT,
+	Block_Shots INT,
+	Points INT,
+	Salary INT,
+	Season VARCHAR,
+	Team VARCHAR
+);
+
+SELECT * FROM players
